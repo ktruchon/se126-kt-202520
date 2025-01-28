8c4d42b2a5d2adfb033101befe69abb3fd337690
ktruchon <43422455+ktruchon@users.noreply.github.com>
2025-01-27 19:19:33 -0500

    Update W4D1 - sequential search documentation

--- class_demos/week4/W4D1_seqSearch_afternoon.py
+++ class_demos/week4/W4D1_seqSearch_afternoon.py
@@ -63,9 +63,50 @@ for i in range(0, len(firstName)):
 print("-----------------------------------------------------------------------------")
 print(f"TOTAL STUDENTS IN FILE: {len(firstName)}")
 
+#SEQUENTIAL SEARCH-------------------------------------------------------------------
+#sequential search: to search for an item by comparing it to all items found within a collection (list)
+#        Searchnig Steps:
+#        1. Set Up & Gaining Search Item from user
+#        2. Perform the searching algorithm
+#        3. Filter and display results: tell user if the item is found or not; if found, display required data
+
+
+'''
+A simple overview of sequential search:
+
+#step 1: set up and gain search
+found = -1 #we are using -1 because it is not a valid index location (technically); you could also use found = "x"
+
+search = input("What are you searching for? ")
+
+#step 2: perform the searching algorithm
+for i in range(0, len(listYouAreSearchingThrough)):
+
+    if search == listYouAreSearchingThrough[i]: 
+        found = i #found drops its original value (-1 or "x") and is replaced with the current index location
+        #since this occurs in the if statement, it will only happen IF what we are looking for matches one of the values on the list we are searching through
+
+#step 3: check and display your results
+if found != -1: #(or "x" if that was your initial value
+    #found has changed, meaning at some point it checked into the if statement within our searching for loop
+    
+    print(f"Your search for {search} has been FOUND!")
+    #display relevant data/results here using 'found' as your index representer 
+    print(f"{listYouAreSearchingThrough[found]}")
+    
+else:
+    #found is still its initial value, meaning we did not find the data the user was looking for
+    #tell the user their search came up empty
+    print(f"Your search for {search} has NOT BEEN FOUND!")
+
+'''
+
+
 #build a repeatable program that allows a user to search through student records by first choosing a search type from a menu, performing the search based on the type, and displaying results
 print("\tWelcome to the Student Search Program")
+
 answer = input("Would you like to start your search? [y/n]: ").lower()
+
 while answer == "y":
     #show user search menu 
     print("\t~Search Menu~")
@@ -78,9 +119,11 @@ while answer == "y":
     #filter search options based on type
     if search_type == "1": #LAST NAME
         #sequential search - search for a student by their LAST name
+        #this version of sequential search is looking for ONE item, a specific and unique LAST name
+        
         print("\tLAST NAME SEARCH~")
         #step 1: set-up and gain search query
-        found = -1  #flag var, will be replaced with index position if name is found
+        found = -1  #flag var, will be replaced with index position if name is found; we are using a -1 because it is not a valid index location
         search_last = input("Enter the last name you wish to find: ") #name we are looking for
 
         #step 2: perform search algo (seq. search -> for loop w/ if statement)
@@ -100,12 +143,16 @@ while answer == "y":
             #NOT found
             print(f"Your search for {search_last} was NOT FOUND!")
             print("Check your cAsInG and sPeLlInG and try again!")
+    
     elif search_type == "2": #LETTER GRADE
         print("\tLETTER GRADE SEARCH")
 
+        #sequential search - search for a collection of students based on their Letter Grade Average
+        #this version of sequential search is looking for MULTIPLE items, based on a specific letter grade
+
         #step 1: set-up and gain search query
-        found = []  #empty list, found locations (index) will be stored if found
-        search_let= input("Enter the LETTER GRADE you wish to find: ") #grade we are looking for all students for
+        found = []  #empty list, found locations (index) will be stored if/when found
+        search_let= input("Enter the LETTER GRADE you wish to find: ") #grade we are looking through all students for
 
         #step 2: perform search algo (seq. search -> for loop w/ if statement)
         for i in range(0, len(let_avg)):
@@ -113,7 +160,7 @@ while answer == "y":
 
             if search_let.upper() == let_avg[i]: 
                 #if performs the SEARCH - is what we're looking for here in the list?
-                found.append(i)  #stores found item's INDEX LOCATION to the found list
+                found.append(i)  #stores found item's INDEX LOCATION to the found list because we may have multiple students whose letter grade fits the searched for grade
                 print(f"Found a {search_let} grade in INDEX {i}")
 
         #step 3: display results to user; make sure you give info: both for found or NOT found
@@ -124,6 +171,8 @@ while answer == "y":
         else: 
             #last name FOUND!
             print(f"Your search for {search_let} was FOUND! Here is their data: ")
+
+            #'found' is a list populated with index locations - we loop through this list, and use found[i] (which again, holds an INDEX from our other searched-through list) to be recalled and used below
             for i in range(0, len(found)):
                 print(f"{found[i]}:  {firstName[found[i]]:10}  {lastName[found[i]]:10}  {test1[found[i]]:3}  {test2[found[i]]:3}  {test3[found[i]]:3}  {num_avg[found[i]]:6.1f}  {let_avg[found[i]]}")
     elif search_type == "3": #exit
@@ -131,7 +180,11 @@ while answer == "y":
         answer = "x"
     else:
         print("\t!INVALID ENTRY!")
+    
     #build a way out of the loop - answer should be able to change value! 
     if search_type == "1" or search_type == "2":
+        #when search_type == "3" the user has chosen to exit, and if they did not provide a 1, 2, or 3 to search_type then they will automatically be brought back through the loop to see the menu again
         answer = input("Would you like to search again? [y/n]: ").lower()
+
+
 print("\nThanks for using the search program. Goodbye!\n")
